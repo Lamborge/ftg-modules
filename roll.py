369a69d6fc5d3b2935c55151293638b072672305
Lamborge <maxpopelov@gmail.com>
2023-06-28 16:13:09 +0300

    roll

---
+++ roll.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+import random
+
+from .. import loader, utils
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class YourMod(loader.Module):
+    """Put the baka"""
+    strings = {"cfg_doc": "",
+                "name": "roll",
+               "after_sleep": "Now you can make baka"}
+
+    @loader.unrestricted  # Security setting to change who can use the command (defaults to owner | sudo)
+    async def rollcmd(self, message):
+        """Roll random number. Syntax: .roll [min number] [max number]"""
+        logger.debug("rolled number")
+        x = message.split(" ")
+        await utils.answer(message, random.randrange(x[1],x[2]))
